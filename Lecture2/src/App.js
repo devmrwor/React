e798f49b01ed2f9b5d634281c0d6f88124123a51
devmrwor <michaelryanrm18@gmail.com>
2020-04-21 20:14:11 +0530

    Lecture 2

---
+++ Lecture2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Person name="Saransh" age="19"/>
+        <Person name="Satvik" age="18"/>
+    </div>
+  );
+}
+
+export default App;
