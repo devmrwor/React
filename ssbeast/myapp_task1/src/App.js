9d55f8b7889a3af468e1dd165eb66adc1b5a483d
devmrwor <michaelryanrm18@gmail.com>
2020-04-23 18:41:18 +0530

    Task_1 completed

--- ssbeast/myapp_task1/src/App.js
+++ ssbeast/myapp_task1/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Person from "./Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return ( <
+      div className = "App" >
+      <
+      Person name = "ssbeast"
+      about = "Pro gamer and Coding Beast" / >
+      <
+      /div>
+    );
+  }
 }
-
 export default App;
