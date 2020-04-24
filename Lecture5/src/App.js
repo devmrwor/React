e88289a19a92aea2e200ecb63a0175b250cf362f
devmrwor <michaelryanrm18@gmail.com>
2020-04-24 16:07:01 +0530

    added Lecture5
    It consists of Task 2.

---
+++ Lecture5/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './App.css';
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
+
+// STATE MANAGEMENT
+
+class App extends Component{
+
+  state = {
+    inputValue : ''
+  };
+
+  inputChangedHandler = (event) =>{
+    this.setState({
+      inputValue: event.target.value 
+    })
+  }
+
+  deleteEventHandler = (index) =>{
+
+    const text = this.state.inputValue.split('');
+
+    text.splice(index,1);
+
+    const updatedText = text.join('');
+
+    this.setState({
+      inputValue: updatedText
+    });
+
+
+  }
+  
+  render(){
+
+    // RENDERING LIST
+
+    let charList = this.state.inputValue.split('').map((ch,index)=>{
+      return (
+        <Char clicked={this.deleteEventHandler} character={ch} key={index} />
+      )
+    });
+
+
+    return (
+      <div className='App'>
+        <h1>React App</h1>
+        <input type='text' onChange={this.inputChangedHandler} value={this.state.inputValue} />
+        <br></br>
+        <Validation inputLength={this.state.inputValue.length}/>
+        {charList}
+      </div>
+    )
+  }
+}
+
+export default App;
