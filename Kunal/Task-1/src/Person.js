76468444b6f862cf32ade1fc4cfa9a7e055d0c8e
devmrwor <michaelryanrm18@gmail.com>
2020-04-25 15:20:17 +0530

    Made a function in Person.js

--- Kunal/Task-1/src/Person.js
+++ Kunal/Task-1/src/Person.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './Person.css';
 
-class Person extends Component {
-    render() {
-        return (
+// class Person extends Component {
+//     render() {
+//         return (
+//             <div className = "Person">
+//                 <h1>Hi , i'm { this.props.name } and i'm {this.props.age } years old.</h1>
+//             </div>
+//         )
+//     }
+// }
+const Person = (props) => {
+    return (
             <div className = "Person">
-                <h1>Hi , i'm { this.props.name } and i'm {this.props.age } years old.</h1>
+                 <h1>Hi , i'm { props.name } and i'm { props.age } years old.</h1>
             </div>
-        )
-    }
+    )
 }
 
 export default Person;
